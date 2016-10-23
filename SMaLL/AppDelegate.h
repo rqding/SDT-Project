fd7bbb23740508d6be4d84fdb29a5cd080ba3c82
Richard <rqding@gmail.com>
2016-10-22 20:52:26 -0400

    Initial Commit

---
+++ SMaLL/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  SMaLL
+//
+//  Created by cj on 2016-10-22.
+//  Copyright © 2016 MathLab Carleton University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ SMaLL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SMaLL
+//
+//  Created by cj on 2016-10-22.
+//  Copyright © 2016 MathLab Carleton University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
